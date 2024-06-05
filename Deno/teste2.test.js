49e9a43b45950a2510877e25c53ead9f170dbe14
João Rafael <joaominchev10@gmail.com>
2024-06-05 11:49:26 -0300

    1°Aula de Código - QTS (Teste Deno)

---
+++ Deno/teste2.test.js
@@ -0,0 +1,15 @@
+import {
+    assertEquals,
+    assertStringIncludes,
+    assertArrayIncludes,
+} from "https://deno.land/std/testing/asserts.ts"
+
+let a = "Oi";
+let b = "Olá";
+
+Deno.test("É oi", () => {
+    assertEquals(a, "Oi")
+})
+Deno.test("É olá", () => {
+    assertEquals(b, "Olá")
+})
